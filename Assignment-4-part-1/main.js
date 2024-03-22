f9f6d0a45ed78e0aab79a678a4c4fbd188dff046
Tyirell <tyirell.garraway@dcmail.ca>
2024-03-22 12:44:05 -0400

    Changed variables to constants

--- Assignment-4-part-1/main.js
+++ Assignment-4-part-1/main.js
@@ -11,42 +11,44 @@ function randomValueFromArray(array){
 
 // 2. RAW TEXT STRINGS
 
-let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
 
-let insertX = ["Willy the Goblin","Big DaddyFather", "Christmas"]
+const insertX = ["Willy the Goblin","Big DaddyFather", "Christmas"];
 
-let insertY = ["the soup kitchen","Disneyland","the White House"]
+const insertY = ["the soup kitchen","Disneyland","the White House"];
 
-let insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"]
+const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
-// 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+// 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION//
 
 randomize.addEventListener('click', result);
 
 function result() {
+    let newStory = storyText;
+
+    const xItem = randomValueFromArray(insertX)
+    const yItem = randomValueFromArray(insertY)
+    const zItem = randomValueFromArray(insertZ)
+
+    newStory = newStory.replaceAll(":insertx:",xItem,);
+    newStory = newStory.replaceAll(":inserty:",yItem,);
+    newStory = newStory.replaceAll(":insertz:",zItem,);
 
   if(customName.value !== '') {
     const name = customName.value;
-    newStory = newStory.replace("Bob",name)
-
+    newStory = newStory.replaceAll("Bob",name);
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const weight = Math.round(300/14) + "stone";
+    const temperature =  Math.round((94-32)/1.8) + "centigrade";
+    newStory = newStory.replace("94 fahrenheit",temperature);
+    newStory = newStory.replace("300 pounds",weight);
 
   }
 
-  story.textContent = "";
+
+  story.textContent = newStory;
   story.style.visibility = 'visible';
-
-  let newStory = storyText
-  let xItem = randomValueFromArray(insertX)
-  let yItem = randomValueFromArray(insertY)
-  let zItem = randomValueFromArray(insertZ)
-
-  newStory = newStory.replaceAll(":insertx:",xItem,)
-  newStory = newStory.replaceAll(":inserty:",yItem,)
-  newStory = newStory.replaceAll(":insertz:",zItem,)
   
 }
