4d57d0e40298734c4e36f50a824816ed79a7ca14
Tyirell <tyirell.garraway@dcmail.ca>
2024-03-22 16:08:00 -0400

    Created objects and loop conditions

--- Assignment-4-part-2/main.js
+++ Assignment-4-part-2/main.js
@@ -1,18 +1,34 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
+
+
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const filenames = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg',]
+
 
 /* Declaring the alternative text for each image file */
-
+const alternative = {
+    'pic1': "Closeup of a human eye",
+    'pic2': "A Giant white wave",
+    'pic3': "Purple and White Flowers",
+    'pic4': "Ancient Ruins",
+    'pic5': "Insect on a Leaf",
+}
 /* Looping through images */
-
+for (let i = 1; i < filenames.length(); i++ ) {
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
+newImage.setAttribute('src', imageLocation);
+newImage.setAttribute('alt', alttext);
 thumbBar.appendChild(newImage);
 
+let imageLocation = `images/pic${i}.jpg`
+let alttext = alternative.pic`${i}`
+
+
+
+}
 /* Wiring up the Darken/Lighten button */
