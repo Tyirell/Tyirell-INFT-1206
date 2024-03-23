825c8230afe82fd1d5cd93345c517793a40a8dbf
Tyirell <tyirell.garraway@dcmail.ca>
2024-03-23 08:43:52 -0400

    Added Collision Detection

--- Assignment-4-part-3/main.js
+++ Assignment-4-part-3/main.js
@@ -57,6 +57,20 @@ class Ball {
     this.y += this.velY
 
   }
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx*dx + dy * dy);
+
+        if(distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+
+  }
 
   
 }
@@ -92,6 +106,7 @@ function loop() {
   for(const ball of balls) {
     ball.draw();
     ball.update();
+    ball.collisionDetect();
   }
 
   requestAnimationFrame(loop);
